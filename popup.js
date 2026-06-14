24434d272054c988590b2dd8356fa64c85eb5848
rxvxrsx <rxvxrsx@users.noreply.github.com>
2026-06-14 11:52:29 +0700

    Fix duplicate PIN in log: check if status already contains PIN before appending

--- popup.js
+++ popup.js
@@ -220,7 +220,7 @@ chrome.runtime.onMessage.addListener(function(message) {
   if (message.status) {
     var t = getStatusType(message.status);
     setStatus(message.status, t);
-    if (message.pinString) {
+    if (message.pinString && message.status.indexOf(message.pinString) === -1) {
       appendLog(message.status + ' PIN: ' + message.pinString);
     } else {
       appendLog(message.status);
